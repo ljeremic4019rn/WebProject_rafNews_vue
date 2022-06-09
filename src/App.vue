5e176c92ad079b437108e35c6e14b69ce1ae8c80
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-06-09 22:15:03 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <Navbar/>
+
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: {Navbar},
+}
+</script>
 
 <style>
 #app {
